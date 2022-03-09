f0dafd69e6e60878c9454a163f81a445e479253a
TDaniAQ <takacs.dani5@gmail.com>
2022-03-09 12:28:03 +0100

    Create 2022_03_09_112216_create_trigger.php
    Trigger

---
+++ database/migrations/2022_03_09_112216_create_trigger.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+                CREATE TRIGGER trigger_1 BEFORE UPDATE ON table_szenzor FOR EACH ROW SET new.KwH = new.KwH - old.KwH
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER trigger_1');
+    }
+};
